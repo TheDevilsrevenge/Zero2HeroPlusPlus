0358efba1a50eee81e043f8fb3016ec7caf64242
TheDevilsrevenge <63287176+TheDevilsrevenge@users.noreply.github.com>
2024-05-27 07:09:46 +0200

    Fixed the mod from also applying to the EoD preset

--- src/mod.ts
+++ src/mod.ts
@@ -4,8 +4,6 @@ import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
 import { LogBackgroundColor } from "@spt-aki/models/spt/logging/LogBackgroundColor";
-
-
 import { readFileSync } from "fs";
 import { join } from "path";
 
@@ -16,22 +14,22 @@ class Mod implements IPostDBLoadMod
         const logger = container.resolve<ILogger>("WinstonLogger");
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
 
-        const profileName = "Joey's Zero to Hero++"
+        const profileName = "Joey's Zero to Hero++";
         
         const tables = databaseServer.getTables();
         
-        const zthProfile = tables.templates.profiles["Edge Of Darkness"]
         
-        const bearInventoryData = JSON.parse(readFileSync(join(__dirname, "./bear_inventory.json"), "utf-8"))
-        const usecInventoryData = JSON.parse(readFileSync(join(__dirname, "./usec_inventory.json"), "utf-8"))
-        
+        const originalZthProfile = tables.templates.profiles["Edge Of Darkness"];
+        const zthProfile = JSON.parse(JSON.stringify(originalZthProfile));
+        const bearInventoryData = JSON.parse(readFileSync(join(__dirname, "./bear_inventory.json"), "utf-8"));
+        const usecInventoryData = JSON.parse(readFileSync(join(__dirname, "./usec_inventory.json"), "utf-8")); 
         zthProfile.bear.character.Inventory = bearInventoryData;
         zthProfile.usec.character.Inventory = usecInventoryData;
 
         tables.templates.profiles[profileName] = zthProfile;
 
-        logger.logWithColor("[Joey's ZTH Mod] Joey's ZTH++ Loaded succesfully", LogTextColor.YELLOW)
+        logger.logWithColor("[Joey's ZTH Mod] Joey's ZTH++ Loaded successfully", LogTextColor.YELLOW);
     }
 }
 
-module.exports = { mod: new Mod() }
+module.exports = { mod: new Mod() };
